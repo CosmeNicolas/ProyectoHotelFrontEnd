ae94cc14a5184acb35915f834dcb01b667b723ed
CosmeNicolas <nicomas7590@gmail.com>
2024-03-27 18:08:21 -0300

    Inicializamos el proyecto hotel

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Container} from 'react-bootstrap'
+function App() {
+
+  return (
+    <>
+     <Container className="main">
+      <h1>Contenido Principal</h1>
+     </Container>
+    </>
+  )
+}
+
+export default App
