9dc673d248696c4822d94e3dc8be70bfdde3a076
CosmeNicolas <nicomas7590@gmail.com>
2024-04-01 20:20:40 -0300

    cambie el logo de la barra de navegacion

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '.././../assets/logo-sf.png'
+import logo from '.././../assets/logo-sinmarco.png'
 import { ImLocation2 } from "react-icons/im";
 
 const NavBar = () => {
