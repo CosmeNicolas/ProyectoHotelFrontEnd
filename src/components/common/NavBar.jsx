0330b0014c29a27ebca72938eef643c718b3cd01
CosmeNicolas <nicomas7590@gmail.com>
2024-04-01 18:26:36 -0300

    agregue estilos a la  barra de navegacion con el logo y sus etiquetas navLink

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,22 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logoTest from '.././../assets/test-logo.png'
+import logo from '.././../assets/logo-sf.png'
 import { ImLocation2 } from "react-icons/im";
 
 const NavBar = () => {
   return (
     <Navbar  data-bs-theme="light"  expand="lg" className="barraDeNavegacion">
     <Container>
-      <Navbar.Brand href="#home"><span><img className='img-nav' src={logoTest} alt="imagen-prueba" /></span></Navbar.Brand>
+      <Navbar.Brand href="#home"><span><img className='img-nav' src={logo} alt="imagen-prueba" /></span></Navbar.Brand>
       <ImLocation2 className='icono-ubicacion' />
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <Nav.Link href="#link">contact</Nav.Link>
-          <Nav.Link href="#link">about us</Nav.Link>
+          <Nav.Link className='fuente-nav' href="#home">Habitaciones</Nav.Link>
+          <Nav.Link className='fuente-nav' href="#link">Galeria</Nav.Link>
+          <Nav.Link className='fuente-nav' href="#link">Quienes Somos</Nav.Link>
+          <Nav.Link className='fuente-nav' href="#link">Contacto</Nav.Link>
+          <Nav.Link className='fuente-nav' href="#link">Login</Nav.Link>
+          <Nav.Link href="#link"><span className='reserva-nav p-2'>Reserva</span></Nav.Link>
+         
         </Nav>
       </Navbar.Collapse>
     </Container>
