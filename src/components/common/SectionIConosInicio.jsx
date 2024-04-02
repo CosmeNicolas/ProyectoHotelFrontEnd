1715b25f0108ea1d66f85d9709c3c450ec6e9eea
CosmeNicolas <nicomas7590@gmail.com>
2024-04-02 14:15:35 -0300

    agreggue las secciones de imagenes y descripciones de los servicios del hotel con sus estilos

---
+++ src/components/common/SectionIConosInicio.jsx
@@ -0,0 +1,31 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { BsSnow, BsBinoculars, BsGem, BsPersonArmsUp } from "react-icons/bs";
+
+const SectionIConosInicio = () => {
+  return (
+    <>
+      <Container>
+        <Row className="iconos-servicios justify-content-center justify-content-md-between mt-5">
+          <Col xs={6} md={3} className="text-center">
+            <BsGem className="iconos-servicios" />
+            <p className="text-center text-icons">Casino</p>
+          </Col>
+          <Col xs={6} md={3} className="text-center mb-3 mb-md-0">
+            <BsBinoculars className="iconos-servicios" />
+            <p className="text-center text-icons">Excursiones</p>
+          </Col>
+          <Col xs={6} md={3} className="text-center">
+            <BsPersonArmsUp className="iconos-servicios" />
+            <p className="text-center text-icons">Spa</p>
+          </Col>
+          <Col xs={6} md={3} className="text-center mb-3 mb-md-0">
+            <BsSnow className="iconos-servicios" />
+            <p className="text-center text-icons">Centro de Ski</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default SectionIConosInicio;
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,6 +1,6 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { BsSnow, BsBinoculars, BsGem, BsPersonArmsUp } from "react-icons/bs";
 import banner from "../../assets/Banner-rollingResort1.gif";
+import SectionIConosInicio from "../common/SectionIConosInicio";
+import SectionImagenInicio from "../common/SectionImagenInicio";
 const Inicio = () => {
   return (
     <>
@@ -13,26 +13,10 @@ const Inicio = () => {
           <h3 className="fuente-slogan-secundario text-center">
             Aventuras, relajación y experiencias únicas te esperan.
           </h3>
-          <Container>
-            <Row className="iconos-servicios justify-content-center justify-content-md-between mt-5">
-              <Col xs={6} md={3} className="text-center">
-                <BsGem className="iconos-servicios" />
-                <p className="text-center text-icons">Casino</p>
-              </Col>
-              <Col xs={6} md={3} className="text-center mb-3 mb-md-0">
-                <BsBinoculars className="iconos-servicios" />
-                <p className="text-center text-icons">Excursiones</p>
-              </Col>
-              <Col xs={6} md={3} className="text-center">
-                <BsPersonArmsUp className="iconos-servicios" />
-                <p className="text-center text-icons">Spa</p>
-              </Col>
-              <Col xs={6} md={3} className="text-center mb-3 mb-md-0">
-                <BsSnow className="iconos-servicios" />
-                <p className="text-center text-icons">Centro de Ski</p>
-              </Col>
-            </Row>
-          </Container>
+         <SectionIConosInicio/>
+         <hr className="mx-4" />
+         <SectionImagenInicio/>
+         <hr className="mx-4" />
         </article>
       </section>
     </>
