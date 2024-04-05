7902a011eb958238f773cf4237e5d747a0c9f259
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-04-04 21:58:17 -0300

    creo css para QuienesSomos tambien agrego imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes , Route } from 'react-router-dom';
 import NavBar from './components/common/NavBar';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
+import './QuienesSomos.css'
 import Inicio from './components/pages/Inicio';
 import Footer from './components/common/Footer';
 import QuienesSomos from './components/pages/QuienesSomos';
--- src/components/pages/QuienesSomos.jsx
+++ src/components/pages/QuienesSomos.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Portada from '../../assets/OIG1.jpg'
+
 
 const QuienesSomos = () => {
     return (
         <>
-            <h1>HOLA QUIENES SOMOS?</h1>
+         <section className="main somos ">
+            <section className='container border '>  
+            <h1> QUIENES SOMOS?</h1>
+            <img className="img-banner img-fluid w-100" src={Portada} alt="Hotel cyber" />
+            <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, ducimus. Dicta illo similique nemo? Omnis architecto, soluta, tempora iusto fuga doloribus cumque velit nam quas officia vitae, neque voluptates eveniet?</article>
+            </section>
+            <div className='bgNombres'>holi</div>
+            </section>
         </>
     );
 };
