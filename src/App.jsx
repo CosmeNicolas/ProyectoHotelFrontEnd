dd2d1c4b9ef50b8de702dc1d655155a194d9eb93
Camila Alani <alanicamila124@gmail.com>
2024-04-06 00:40:51 -0300

    agregue el maquetado del error 404, con una imagen y un mensaje carismatico para el usuario, finalmente un boton que le permite ir al inicio y sus estilos correspondientes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Inicio from './components/pages/Inicio';
 import Footer from './components/common/Footer';
+import Error404 from './components/pages/Error404';
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
    <NavBar/>
    <Routes>
     <Route exact path='/' element={<Inicio/>}/>
+    <Route path='*' element={<Error404/>}/>
    </Routes>
    <Footer></Footer>
    </BrowserRouter>
