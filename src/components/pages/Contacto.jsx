5e149c787e37cf734557e18f431b9674de4637c7
nachocode <lic.medinajimenez@gmail.com>
2024-04-15 18:47:10 -0300

    se saco emailJS

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -1,34 +1,13 @@
 import { Button, Row, Col, Container, Image, Form, InputGroup, FloatingLabel } from "react-bootstrap";
 import pileta from "../../assets/pileta.jpeg";
 import { BsFillGeoAltFill } from "react-icons/bs";
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
 
 const Contacto = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
-
 
   return (
     
       <section>
-        <div Container>
+        <div>
           <Image
             src={pileta}
             alt="pileta"
@@ -45,24 +24,23 @@ const Contacto = () => {
           <Col xs={12} sm={12} lg={5} >
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1028.2928906921807!2d-65.2075942400546!3d-26.836748847013748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1695685464063!5m2!1ses-419!2sar"
-              style={{ border: "0" }}
-              allowfullscreen=""
+              style={{ border: "0" }}             
               loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
+              referrerPolicy="no-referrer-when-downgrade"
               width="100%"
               height="100%"
             ></iframe>
           </Col>
           <Col  xs={12} sm={12} lg={6} className="texto-FormContacto py-3">
             <h4><BsFillGeoAltFill className="icono-ubicacionContacto"/>Av. Gral. Paz 576, San Miguel de Tucuman, CP 4000</h4>
-            <Form ref={form} onSubmit={sendEmail} className="text-center">
+            <Form className="text-center">
               <Form.Group>
                 <InputGroup className="py-3">
                 <InputGroup.Text id="nombreContacto">Nombre</InputGroup.Text>
                 <Form.Control
                 placeholder="Juan"
                 aria-label="nombre"
-                name="user_name"
+              
                 />
                 </InputGroup>
                 <InputGroup>
@@ -78,7 +56,7 @@ const Contacto = () => {
                 placeholder="email@email.com"
                 aria-label="email"
                 type="email"
-                name="user_email"
+               
                 />
                 </InputGroup>
                 <FloatingLabel controlId="consultaContacto" label="Consulta">
@@ -86,7 +64,7 @@ const Contacto = () => {
           as="textarea"
           placeholder="Deje su consulta"
           style={{ height: '100px' }}
-          name="message"
+          
         />
       </FloatingLabel>
           
@@ -103,3 +81,4 @@ const Contacto = () => {
 };
 
 export default Contacto;
+
