a782351cf8892e4bb81c451a20e87e363f439dc2
emir paradi <emir.alejandro.paradi@gmail.com>
2024-04-15 17:44:30 -0300

    Se agregaron las validaciones del formulario con react-hook-form

--- src/components/pages/habitaciones/FormularioHabitacion.jsx
+++ src/components/pages/habitaciones/FormularioHabitacion.jsx
@@ -1,130 +1,187 @@
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Card, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const FormularioHabitacion = () => {
-  //! --------------------------------- Variables globales ---------------------
   const {
     register,
     handleSubmit,
     reset,
-    setValue,
     formState: { errors },
+    getValues,
   } = useForm();
 
-  //! -------------------------------- Funciones -------------------------------
+  const habitacionValida = async (habitacion) => {
+    console.log(habitacion);
+    reset();
+  };
 
-  //! --------------------------------- Maquetado ------------------------------
   return (
     <section className="fondo-login">
-      <Container className="px-lg-4 py-4 container-login">
-        <h1 className="fuente-login text-center text-light ">
-          Crear Habitación
-        </h1>
-        <Form className=" rounded-2 px-lg-5 pt-3 pb-5">
-         
-          <Form.Group
-            className="mb-3 text-light"
-            controlId="producto"
-          >
-            <Form.Label>Producto*</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ej: Cafe"
-              className="color-inputs"
-            />
-            <Form.Text className="text-danger">
-              {errors.nombreProducto?.message}
-            </Form.Text>
-          </Form.Group>
+      <div className="d-flex justify-content-center mt-5">
+        <Card className="p-5 container-formulario-Usuario my-5">
+          <h1 className="fuente-login text-center text-light mb-5">
+            Crear Habitación
+          </h1>
+          <Form className="rounded-2" onSubmit={handleSubmit(habitacionValida)}>
+            <Form.Group className="mb-3 text-light" controlId="numero">
+              <Form.Label>Número de Habitación*</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Ej: 227"
+                className="color-inputs"
+                {...register("numero", {
+                  required: "El número de la habitación es obligatorio",
+                  min: {
+                    value: 100,
+                    message:
+                      "Debe ingresar apartir de la habitación número 100",
+                  },
+                  max: {
+                    value: 10000,
+                    message: "Debe ingresar hasta la habitación número 10.000",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.numero?.message}
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group
-            className="mb-3 text-light"
-            controlId="precio"
-          >
-            <Form.Label>Precio*</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Ej: 50"
-              className="color-inputs"
-            />
-            <Form.Text className="text-danger">
-              {errors.precio?.message}
-            </Form.Text>
-          </Form.Group>
+            <Form.Group className="mb-3 text-light" controlId="tipo">
+              <Form.Label>Categoría*</Form.Label>
+              <Form.Select
+                aria-label="select"
+                className="color-inputs text-secondary"
+                {...register("tipo", {
+                  required:
+                    "Seleccionar la categoría de la habitación es obligatorio",
+                })}
+              >
+                <option value={""}>Seleccione una opción</option>
+                <option value={"Doble Twin"}>Doble Twin</option>
+                <option value={"Doble Superior"}>Doble Superior</option>
+                <option value={"Triple Superior"}>Triple Superior</option>
+                <option value={"Suite"}>Suite</option>
+              </Form.Select>
+              <Form.Text className="text-danger">
+                {errors.tipo?.message}
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group
-            className="mb-3 text-light"
-            controlId="url"
-          >
-            <Form.Label>Imagen URL*</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ej: https://images.pexels.com/photos/3892469/pexels-photo-3892469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              className="color-inputs"
-            />
-            <Form.Text className="text-danger">
-              {errors.imagen?.message}
-            </Form.Text>
-          </Form.Group>
+            <Form.Group className="mb-3 text-light" controlId="precio">
+              <Form.Label>Precio*</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Ej: 50000"
+                className="color-inputs"
+                {...register("precio", {
+                  required: "El precio de la habitación es obligatorio",
+                  min: {
+                    value: 27000,
+                    message:
+                      "El monto de la habitación debe ser mayor a $27.000",
+                  },
+                  max: {
+                    value: 120000,
+                    message:
+                      "El monto de la habitación debe ser menor a $120.000",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.precio?.message}
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group
-            className="mb-3 text-light"
-            controlId="categoria"
-          >
-            <Form.Label>Categoria*</Form.Label>
-            <Form.Select
-              aria-label="select"
-              /* required */
-              className="color-inputs text-secondary"
-            >
-              <option value={""}>Seleccione una opcion</option>
-              <option value={"Panaderia"}>Panaderia</option>
-              <option value={"Cafeteria"}>Cafeteria</option>
-              <option value={"Reposteria"}>Reposteria</option>
-            </Form.Select>
-            <Form.Text className="text-danger">
-              {errors.categoria?.message}
-            </Form.Text>
-          </Form.Group>
+            <Form.Group className="mb-3 text-light" controlId="fechaIngreso">
+              <Form.Label>Fecha de ingreso*</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Ej: 27/03/2024"
+                className="color-inputs"
+                {...register("fechaIngreso", {
+                  required: "La fecha de ingreso es obligatoria",
+                  validate: {
+                    fechaFutura: (value) => {
+                      const fechaIngreso = new Date(value);
+                      const hoy = new Date();
+                      return fechaIngreso >= hoy || "Ingrese una fecha válida";
+                    },
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.fechaIngreso?.message}
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group
-            className="mb-3 text-light"
-            controlId="descripcionBreve"
-          >
-            <Form.Label>Descripcion breve*</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ej: Una taza de café suave y aromática"
-              className="color-inputs"
-            />
-            <Form.Text className="text-danger">
-              {errors.descripcionBreve?.message}
-            </Form.Text>
-          </Form.Group>
+            <Form.Group className="mb-3 text-light" controlId="fechaSalida">
+              <Form.Label>Fecha de salida*</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Ej: 27/03/2024"
+                className="color-inputs"
+                {...register("fechaSalida", {
+                  required: "La fecha de salida es obligatoria",
+                  validate: {
+                    fechaFutura: async (value) => {
+                      const fechaSalida = await new Date(value);
+                      const hoy = new Date();
+                      return (
+                        fechaSalida >= hoy ||
+                        "La fecha debe ser posterior a la fecha de hoy"
+                      );
+                    },
+                    fechaPosterior: async (value) => {
+                      const fechaIngreso = await new Date(
+                        getValues("fechaIngreso")
+                      );
+                      const fechaSalida = await new Date(value);
+                      return (
+                        fechaSalida > fechaIngreso ||
+                        "La fecha de salida debe ser posterior a la fecha de ingreso"
+                      );
+                    },
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.fechaSalida?.message}
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group
-            className="mb-3 text-light"
-            controlId="descripcionAmplia"
-          >
-            <Form.Label>Descripcion detallada*</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              placeholder="El café es una bebida aromática y vigorizante que se obtiene de los granos tostados de la planta de café. Su sabor distintivo y su rica historia lo convierten en una de las bebidas más populares y apreciadas en todo el mundo. Al ser versátil, el café se presenta en una amplia variedad de formas, desde el clásico espresso hasta las elaboradas mezclas de cafés gourmet."
-              className="formularioTextArea color-inputs"
-            />
-            <Form.Text className="text-danger">
-              {errors.descripcion?.message}
-            </Form.Text>
-          </Form.Group>
+            <Form.Group className="mb-3 text-light" controlId="imagen">
+              <Form.Label>Imagen URL*</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ej: https://images.pexels.com/photos/3892469/pexels-photo-3892469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                className="color-inputs"
+                {...register("imagen", {
+                  required:
+                    "El campo para ingresar la URL de la imagen es obligatorio",
+                  pattern: {
+                    value: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)/,
+                    message: "Debe ingresar una URL válida (jpg|gif|png|jpeg)",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.imagen?.message}
+              </Form.Text>
+            </Form.Group>
 
-          <div className="text-end">
-            <Button type="submit" variant="dark" className="border-0 boton-login">
-              Guardar
-            </Button>
-          </div>
-        </Form>
-      </Container>
+            <div className="text-start">
+              <Button
+                type="submit"
+                variant="dark"
+                className="border-0 mt-3 boton-login"
+              >
+                Guardar
+              </Button>
+            </div>
+          </Form>
+        </Card>
+      </div>
     </section>
   );
 };
