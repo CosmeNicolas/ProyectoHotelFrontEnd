cb97be060a849e86ff5a63ceef713e6cf6f6d9e7
emir paradi <emir.alejandro.paradi@gmail.com>
2024-04-18 19:26:23 -0300

    Se fixeo la logica para borrar un usuario buscado por su identificador único

--- src/components/pages/usuario/ItemUsuario.jsx
+++ src/components/pages/usuario/ItemUsuario.jsx
@@ -4,46 +4,68 @@ import { MdDelete } from "react-icons/md";
 import Swal from "sweetalert2";
 import { eliminarUsuarioAPI, leerUsuariosAPI } from "../../../helpers/queries";
 
-
-const ItemUsuario = ({usuariosAdmin, setUsuarioAdmin, actualizarUsuarios}) => {
-
-  const eliminarUsuario = async () => {
-    const respuesta = await eliminarUsuarioAPI(usuariosAdmin._id);
-      if(respuesta.status === 200){
-        const usuarioActualizado = leerUsuariosAPI()
-        setUsuarioAdmin(usuarioActualizado)
-        console.log(usuarioActualizado)
-      }else{
-        console.log("tenemos un error")
+const ItemUsuario = ({ usuariosAdmin, setUsuarioAdmin }) => {
+  //!-------------------- Funciones -------------------------
+  const eliminarUsuario = async (id, usuario) => {
+    Swal.fire({
+      html: `¿Estas seguro que deseas borrar al usuario <span class="text-danger">${usuario}</span> ?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const respuesta = await eliminarUsuarioAPI(id);
+        if (respuesta.status === 200) {
+          const usuariosActualizados = await leerUsuariosAPI();
+          setUsuarioAdmin(usuariosActualizados);
+          Swal.fire({
+            title: "Borrado!",
+            html: `Su usuario: <span class="text-danger">${usuario}</span> ha sido borrada!`,
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            title: "Ops!",
+            text: `Se produjo un error intente mas tarde`,
+            icon: "error",
+          });
+        }
       }
-    }
-  
+    });
+  };
 
-
+  //!-------------------- Maquetado -------------------------
   return (
     <>
-    {usuariosAdmin.map((usuarioAdmin)=> (
-     <tr key={usuarioAdmin._id}> 
-         <td>#</td>
-              <td>{usuarioAdmin.nombreCompleto}</td>
-              <td>{usuarioAdmin.email}</td>
-              <td>{usuarioAdmin.usuario}</td>
-          
-              <td>{usuarioAdmin.rol}</td>
-              <td>{usuarioAdmin.suspendido? 'Desabilitado❌' : 'Habilitado✅'}</td>
-                <td>
-                <Button variant="warning" className="p-3 mx-1" >
-                <FaEdit  />
-                </Button>
-                <Button variant="danger" className="p-3 mx-1 text-center" onClick={eliminarUsuario}>
-                <MdDelete />
-                </Button>
-                </td>
-    </tr>
-))
-    }
+      {usuariosAdmin.map((usuarioAdmin) => (
+        <tr key={usuarioAdmin._id}>
+          <td>#</td>
+          <td>{usuarioAdmin.nombreCompleto}</td>
+          <td>{usuarioAdmin.email}</td>
+          <td>{usuarioAdmin.usuario}</td>
+          <td>{usuarioAdmin.rol}</td>
+          <td>{usuarioAdmin.suspendido ? "Desabilitado❌" : "Habilitado✅"}</td>
+          <td>
+            <Button variant="warning" className="p-3 mx-1">
+              <FaEdit />
+            </Button>
+            <Button
+              variant="danger"
+              className="p-3 mx-1 text-center"
+              onClick={() =>
+                eliminarUsuario(usuarioAdmin._id, usuarioAdmin.usuario)
+              }
+            >
+              <MdDelete />
+            </Button>
+          </td>
+        </tr>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default ItemUsuario
+export default ItemUsuario;
