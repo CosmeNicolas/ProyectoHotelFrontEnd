e35b293a3c79e7ac83972a718afb9d0fadb1d5ed
emir paradi <emir.alejandro.paradi@gmail.com>
2024-04-18 21:00:49 -0300

    Se agregaron estilos acorde a la página

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -1,31 +1,32 @@
-import { Button, Row, Col, Container, Image, Form, InputGroup, FloatingLabel } from "react-bootstrap";
+import {
+  Button,
+  Row,
+  Col,
+  Container,
+  Image,
+  Form,
+  InputGroup,
+  FloatingLabel,
+} from "react-bootstrap";
 import pileta from "../../assets/pileta.jpeg";
 import { BsFillGeoAltFill } from "react-icons/bs";
 
-
-
 const Contacto = () => {
   return (
-  
-      <section>
-        <div>
-          <Image
-            src={pileta}
-            alt="pileta"
-            className="img-piletaContacto"
-            fluid
-          />
-          <div className="texto-tituloContacto text-center">
-            <h2>Gracias por elegirnos</h2>
-            <p>Lo estaremos esperando</p>
-          </div>
+    <section>
+      <div>
+        <Image src={pileta} alt="pileta" className="img-piletaContacto" fluid />
+        <div className="texto-tituloContacto text-center">
+          <h2>Gracias por elegirnos</h2>
+          <p>Lo estaremos esperando</p>
         </div>
+      </div>
       <Container className="py-5">
         <Row className="justify-content-center">
-          <Col xs={12} sm={12} lg={5} >
+          <Col xs={12} sm={12} lg={5}>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1028.2928906921807!2d-65.2075942400546!3d-26.836748847013748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1695685464063!5m2!1ses-419!2sar"
-              style={{ border: "0" }}             
+              style={{ border: "0" }}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
               width="100%"
@@ -33,57 +34,51 @@ const Contacto = () => {
             ></iframe>
           </Col>
 
-          <Col  xs={12} sm={12} lg={6} className="texto-FormContacto py-3">
-            <h4><BsFillGeoAltFill className="icono-ubicacionContacto"/>Av. Gral. Paz 576, San Miguel de Tucuman, CP 4000</h4>
+          <Col xs={12} sm={12} lg={6} className="texto-FormContacto py-3">
+            <h4>
+              <BsFillGeoAltFill className="icono-ubicacionContacto" />
+              Av. Gral. Paz 576, San Miguel de Tucuman, CP 4000
+            </h4>
             <Form className="text-center">
               <Form.Group>
                 <InputGroup className="py-3">
-                <InputGroup.Text id="nombreContacto">Nombre</InputGroup.Text>
-                <Form.Control
-                placeholder="Juan"
-                aria-label="nombre"
-
-                />
+                  <InputGroup.Text id="nombreContacto">Nombre</InputGroup.Text>
+                  <Form.Control placeholder="Juan" aria-label="nombreContacto" className="color-inputs"/>
                 </InputGroup>
                 <InputGroup>
-                <InputGroup.Text id="apellidoContacto">Apellido</InputGroup.Text>
-                <Form.Control
-                placeholder="Garcia"
-                aria-label="apellido"
-                />
+                  <InputGroup.Text id="apellidoContacto">
+                    Apellido
+                  </InputGroup.Text>
+                  <Form.Control placeholder="Garcia" aria-label="apellidoContacto" className="color-inputs"/>
                 </InputGroup>
                 <InputGroup className="py-3">
-                <InputGroup.Text id="emailContacto">E-mail</InputGroup.Text>
-                <Form.Control
-                placeholder="email@email.com"
-                aria-label="email"
-                type="email"
-
-                />
-
+                  <InputGroup.Text id="emailContacto">E-mail</InputGroup.Text>
+                  <Form.Control
+                    placeholder="email@email.com"
+                    aria-label="email"
+                    type="email"
+                    className="color-inputs"
+                  />
                 </InputGroup>
                 <FloatingLabel controlId="consultaContacto" label="Consulta">
-        <Form.Control
-          as="textarea"
-          placeholder="Deje su consulta"
-          style={{ height: '100px' }}
-  />
-      </FloatingLabel>
-          
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Deje su consulta"
+                    className="formularioTextArea color-inputs"      
+                    style={{ height: "150px" }}
+                  />
+                </FloatingLabel>
               </Form.Group>
 
-              <Button type="submit" className="btn-Contacto my-3">
+              <Button type="submit" className="btn-Contacto my-3" variant="dark">
                 Enviar
               </Button>
             </Form>
           </Col>
         </Row>
       </Container>
-     </section>
-
-
+    </section>
   );
 };
 
 export default Contacto;
-
